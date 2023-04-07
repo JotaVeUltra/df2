edcb5f6a7e7bf7fed646b1fac291546d47f13cef
João Marques <jotaveultra@gmail.com>
2023-04-07 00:29:31 -0300

    Show error message if argument is not a directory

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,11 @@ fn handle<W: Write>(writer: &mut W, args: Vec<String>) {
                 writer,
                 vec![b"Error: Directory ", arg.as_bytes(), b" does not exist.\n"],
             );
+        } else if !path.is_dir() {
+            return write_output(
+                writer,
+                vec![b"Error: ", arg.as_bytes(), b" is not a directory.\n"],
+            );
         }
     }
 }
@@ -38,7 +43,10 @@ fn write_output<W: Write>(writer: &mut W, texts: Vec<&[u8]>) {
 
 #[cfg(test)]
 mod tests {
-    use std::str::from_utf8;
+    use std::{
+        fs::{self, File},
+        str::from_utf8,
+    };
 
     use super::*;
 
@@ -85,5 +93,25 @@ mod tests {
 
         // Assertions
         assert!(out.contains("Error: Directory nonexistent_dir does not exist.\n"));
+    }
+
+    #[test]
+    fn handle_writes_error_message_if_argument_is_not_a_directory() {
+        // Setup
+        let mut buf = Vec::new();
+        let file_path = "file.txt";
+        let args: Vec<String> = vec![String::from("bin"), String::from(file_path)];
+        File::create(file_path).unwrap();
+
+        // Test
+        handle(&mut buf, args);
+        let out = from_utf8(&buf).unwrap();
+
+        // Assertions
+        let expected_output = format!("Error: {} is not a directory.\n", file_path);
+        assert_eq!(expected_output, out);
+
+        // Teardown
+        fs::remove_file(file_path).unwrap();
     }
 }
